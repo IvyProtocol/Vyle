5f5f865526204def1804fd1564c644bc0538dd69
IvyProtocol <lizakhan5909@gmail.com>
2026-03-27 03:31:06 +0600

    Broken exec-start.sh, fixed

--- Configs/.local/bin/exec-start.sh
+++ Configs/.local/bin/exec-start.sh
@@ -4,10 +4,6 @@ source "$scrDir/globalcontrol.sh"
 
 flag="${VYLE_CACHE_HOME}/done"
 
-if ! pgrep -x "swww-daemon" >/dev/null; then
-  swww-daemon &
-fi
-
 if [[ ! -e "$flag" ]]; then
   "${scrDir}/swwwallswitch.sh" -t -i "${VYLE_CONFIG_HOME}/theme/${VYLE_RESERVED_THEME}/wallpapers/${wallSet##*/}" -n --s -w --swww-n >/dev/null 2>&1
   touch "$flag"
