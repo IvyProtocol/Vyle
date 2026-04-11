03370cb2cb2ca52c136c4cc7e10b733334c8a05c
Vyle <121424993+IvyProtocol@users.noreply.github.com>
2026-04-11 16:25:16 +0600

    Update config.fish

--- Configs/.config/fish/config.fish
+++ Configs/.config/fish/config.fish
@@ -3,7 +3,6 @@ if status is-interactive
     starship init fish | source
 
     alias pamcan pacman
-    alias env_pkg="$HOME/.config/fish/fish_scripts/env_pkg.sh"
     alias zimg="kitty +kitten icat"
     set EDITOR nvim
 end
